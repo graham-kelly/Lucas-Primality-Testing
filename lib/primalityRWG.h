aa2337373d2a2d60680a78a628280d6e80155f69
graham-kelly <graham.l.kelly@gmail.com>
2019-08-24 17:22:40 -0600

    Add files via upload

--- lib/primalityRWG.h
+++ lib/primalityRWG.h
@@ -1,19 +1,12 @@
 #ifndef PRIMALITYRWG_H_
 #define PRIMALITYRWG_H_
 
-#ifndef PRIMALITY_TEST_2_8_H_
-#define PRIMALITY_TEST_2_8_H_
+#ifndef PRIMALITY_TEST_2_H_
+#define PRIMALITY_TEST_2_H_
 
-int primality_test_2_8 (mpz_t A, int r, int n, short y);
+int primality_test_2 (mpz_t A, int r, int n, short y);
 
-#endif		//PRIMALITY_TEST_2_8_H_
-
-#ifndef PRIMALITY_TEST_2_10_H_
-#define PRIMALITY_TEST_2_10_H_
-
-int primality_test_2_10 (mpz_t A, int n, short y);
-
-#endif		//PRIMALITY_TEST_2_10_H_
+#endif		//PRIMALITY_TEST_2_H_
 
 #ifndef GET_U_I_H_
 #define GET_U_I_H_
@@ -36,19 +29,12 @@ _Bool get_v_i (mpz_t result, mpz_t i, int p, int d, mpz_t N);
 
 #endif		//GET_V_I_H_
 
-#ifndef PRIMALITY_TEST_7_2_4_H_
-#define PRIMALITY_TEST_7_2_4_H_
+#ifndef PRIMALITY_TEST_7_H_
+#define PRIMALITY_TEST_7_H_
 
-int primality_test_7_2_4 (mpz_t A, int r, int n, int eta);
+int primality_test_7 (mpz_t A, int r, int n, int eta);
 
-#endif		//PRIMALITY_TEST_7_2_4_H_
-
-#ifndef PRIMALITY_TEST_7_5_H_
-#define PRIMALITY_TEST_7_5_H_
-
-int primality_test_7_5 (mpz_t A, int n, int eta);
-
-#endif		//PRIMALITY_TEST_7_5_H_
+#endif		//PRIMALITY_TEST_7_H_
 
 #ifndef GET_CUV_I_H_
 #define GET_CUV_I_H_
